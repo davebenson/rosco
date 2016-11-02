048bcb3ce5ebbdd156c4c333154a4cec850548a3
Dave Benson <daveb@ffem.org>
2016-11-01 17:12:01 -0700

    ..

--- src/rosco-node.h
+++ src/rosco-node.h
@@ -1,13 +1,4 @@
 
-typedef struct RoscoURL RoscoURL;
-RoscoURL      *rosco_url_new           (const char   *str,
-                                        RoscoError  **error);
-RoscoURL      *rosco_url_new_append    (RoscoURL     *url,
-                                        const char   *format,
-                                        ...);
-void           rosco_url_ref           (RoscoURL     *url);
-void           rosco_url_unref         (RoscoURL     *url);
-
 typedef struct RoscoNode RoscoNode;
 typedef struct RoscoNodeFuncs {
   void (*disconnected)  (RoscoNode  *node, 
@@ -22,22 +13,24 @@ typedef struct RoscoNodeFuncs {
                          void       *funcs_data);
 } RoscoNodeFuncs;
 
-RoscoNode          *rosco_node_new              (RoscoURL             *master_url,
+RoscoNode          *rosco_node_new              (DskURL               *master_url,
                                                  const RoscoNodeFuncs *funcs, 
                                                  void                 *funcs_data,
                                                  RoscoDestroyFunc      funcs_data_destroy);
 
 // register service implementations
+typedef void (*RoscoClosure) (RoscoMessage *output,
+                              void         *closure_data);
 typedef void (*RoscoServiceFunc)  (RoscoService     *service,
                                    RoscoMessage     *input,
                                    RoscoClosure      response_callback,
-                                   void             *response_callback_data);
+                                   void             *closure_data);
                                    
 
 RoscoService       *rosco_node_register_service (RoscoNode            *node,
                                                  const char           *service_name,
-                                                 RoscoMessageType     *input,           //???
-                                                 RoscoMessageType     *output,          //???
+                                                 RoscoMessageType     *input,
+                                                 RoscoMessageType     *output,
                                                  RoscoServiceFunc      func,
                                                  void                  func_data,
                                                  RoscoDestroyFunc      func_data_destroy);
