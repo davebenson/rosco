2a8e675ecd311d3b5bff88ea02a526c0cdff6420
Dave Benson <daveb@ffem.org>
2016-11-10 14:36:24 -0800

    ...

--- src/rosco-node.c
+++ src/rosco-node.c
@@ -1,6 +1,7 @@
 #include "dsk/dsk.h"
 
-typedef void (*XmlrpcHandler) (DskXml *response,
+typedef void (*XmlrpcHandler) (DskError *error,
+                               DskXml *response,
                                void *handler_data);
 
 static void
@@ -10,6 +11,33 @@ do_xmlrpc (DskUrl *url,
            XmlrpcHandler handler,
            void *handler_data)
 {
+  DskClientStreamOptions client_stream_options = DSK_CLIENT_STREAM_OPTIONS_INIT;
+  DskOctetSink *sink;
+  DskOctetSource *source;
+  DskError *error = NULL;
+  if (!dsk_client_stream_new (&client_stream_options, NULL, &sink, &source, &error))
+    {
+      handler (error, NULL, handler_data);
+      dsk_error_unref (error);
+      return;
+    }
+
+  DskHttpClientStreamOptions http_client_stream_options = DSK_HTTP_CLIENT_STREAM_OPTIONS_INIT;
+  DskHttpClientStream *http_client = dsk_http_client_stream_new (sink, source, &http_client_stream_options);
+  DskHttpClientStreamRequestOptions req_options = DSK_HTTP_CLIENT_STREAM_REQUEST_OPTIONS_INIT;
+  req_options.post_data_len = ...
+  req_options.post_data_slab = ...
+  req_options.
+  dsk_http_client_stream_request (...);
+  dsk_object_unref (sink);
+  dsk_object_unref (source);
+  dsk_object_unref (http_client);
+}
+
+DskHttpClientStream *
+dsk_http_client_stream_new     (DskOctetSink        *sink,
+                                DskOctetSource      *source,
+                                const DskHttpClientStreamOptions *options);
   RoscoHttpRequest *request = rosco_http_request_new ("POST", ...);
   rosco_http_request_add_header (request, "Content-Type", "text/xml");
   RoscoBinaryData *data = rosco_mutable_xml_serialize (input);
